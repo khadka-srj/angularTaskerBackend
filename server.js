5c7be4cb67ebd84593c19266e9031e92960ec7f1
khadka-srj <83262160+khadka-srj@users.noreply.github.com>
2023-10-09 10:32:39 +0545

    added user login route

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ let dataBase = [
     day: "2023-10-24T18:15:00.000Z",
   },
 ];
+let users = [
+  {
+    id: 1,
+    email: "suraj@gmail.com",
+    name: "suraj",
+    password: "1234",
+  },
+  {
+    id: 2,
+    email: "saitama@gmail.com",
+    name: "saitama",
+    password: "1234",
+  },
+];
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -24,12 +39,9 @@ app.get("/", function (req, res) {
 });
 
 app.delete("/:id", function (req, res) {
-  const id = req.params.id;
-  const taskToDelete = dataBase.find((t) => t.id == id);
-  dataBase.splice(
-    dataBase.findIndex((a) => a.id === id),
-    1
-  );
+  const id = parseInt(req.params.id);
+  const taskToDelete = dataBase.find((t) => t.id === id);
+  dataBase = dataBase.filter((data) => data.id !== id);
   res.status(200).json(taskToDelete);
 });
 
@@ -47,4 +59,21 @@ app.put("/:id", function (req, res) {
   dataBase[taskToUpdateIndex] = updatedTask;
   res.status(200).json(updatedTask);
 });
+
+// user route
+app.post("/auth", function (req, res) {
+  const existingUser = users.find((user) => user.email === req.body.email);
+  if (existingUser && existingUser.password === req.body.password) {
+    res.status(200).json({
+      id: existingUser.id,
+      email: existingUser.email,
+      name: existingUser.name,
+    });
+  } else {
+    console.log(existingUser);
+    console.log(req.body);
+    res.status(400).json({ message: "Email or password is incorrect." });
+  }
+});
+
 app.listen(5000);
